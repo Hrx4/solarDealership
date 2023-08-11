c52f214ad561595f7f10c08e4a7f3e55c1f141f3
Kamal <dragkamal71@gmail.com>
2023-08-11 22:23:39 +0530

    added

--- src/routes/customer/profile.jsx
+++ src/routes/customer/profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Button } from '@mui/material'
 import { useNavigate } from 'react-router-dom';
 import { useSignOut } from 'react-auth-kit';
@@ -8,15 +8,9 @@ const Profile = () => {
   const signout = useSignOut()
   const navigate = useNavigate()
 
-  const [change, setChange] = useState("profile")
     const data = JSON.parse(localStorage.getItem("customerData"))
 
-    const handleProfile = (e)=>{
-      setChange("profile")
-    }
-    const handleFinance = (e)=>{
-      setChange("")
-    }
+    
     const handleLogout = (e)=>{
       localStorage.removeItem("customerData")
       signout();
@@ -27,58 +21,45 @@ const Profile = () => {
 
   return (
 
-    <>
-    {
-      (localStorage.getItem("customerData")===null) 
-    }
-    
-    <div className="adminHeader">
-  <h1>
-  Hello! {data.name}
-  </h1>
-</div>
-    <div className='admin'>
-        <Button variant='contained' color='success'   onClick={()=> handleProfile()} >
-            Profile
-        </Button>
-        <Button variant='contained' color='success'   onClick={()=> handleFinance()} >
-          Finance
-        </Button>
-        <Button variant='contained' color='success'   onClick={()=> handleLogout()} >
-          Log Out
-        </Button>
-    </div>
 
-        <div className="about-me-card">.
-        <div className="profile-picture">
-            <img src={data.photo} alt="Profile" />
-        </div>
-        {
-          (change) ?          
-        
-    <div className="profile-details">
-        <h2>{data.name}</h2>
-        <p>Registration No: {data.registrationNo}</p>
-        <p>Email: {data.email}</p>
-        <p>Mobile No: {data.mobileNo}</p>
-        <p>Aadhar No: {data.aadharNo}</p>
-        <p>PAN No: {data.panNo}</p>
-        <p>Dist Name: {data.distName}</p>
-        <p>Landmark: {data.landMark}</p>
-        <p>Address: {data.address}</p>
-    </div>
-         :
-         <div className="finance-details">
-        
-        <p>account No: {data.accountNo}</p>
-        <p>ifsc Code: {data.ifscCode}</p>
-        <p>registration Pay: {data.registrationPay}</p>
-        
-    </div>
-        }
-        
-        </div>
-    
+    <>
+         <div className="customerbody">
+            <div className="customerheader">
+              <div className="upper">
+              <div className='customerbtn'>
+              <Button variant='contained' color='error'   onClick={()=> handleLogout()} >
+               Log Out
+             </Button>
+              </div>
+              </div>
+              <div className="profile-picture">
+                <img src={data.photo} alt="Profile" />
+            </div>
+            </div>
+            <div className="customermain">
+              <h2>{data.name}</h2>
+
+                  <div className="customercontent">
+                    <div className="leftside">
+                    <p>Registration No: {data.registrationNo}</p>
+                    <p>Email: {data.email}</p>
+                    <p>Mobile No: {data.mobileNo}</p>
+                    <p>Aadhar No: {data.aadharNo}</p>
+                    <p>PAN No: {data.panNo}</p>
+                    <p>Dist Name: {data.distName}</p>
+                    <p>Landmark: {data.landMark}</p>
+                    <p>Address: {data.address}</p>
+                    </div>
+
+          
+                    <div className="rightside">
+                    <p>account No: {data.accountNo}</p>
+                    <p>ifsc Code: {data.ifscCode}</p>
+                    <p>registration Pay: {data.registrationPay}</p>
+                    </div>
+                  </div>
+            </div>
+         </div>
     </>
   )
 }
