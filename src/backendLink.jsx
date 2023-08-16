6fbdaafa6fe626393241abf86aa2ae9f96842fdc
Kamal <dragkamal71@gmail.com>
2023-08-16 22:08:44 +0530

    Added

--- src/backendLink.jsx
+++ src/backendLink.jsx
@@ -1,5 +1,5 @@
-// const backend = "https://solarbackend.onrender.com"
-const backend = "http://localhost:5000"
+const backend = "https://solarbackend.onrender.com"
+// const backend = "http://localhost:5000"
 
 
 export default backend;
