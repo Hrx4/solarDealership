1d59f6c616954e6a8155a1b3d0e6e18373e0fbe6
Kamal <dragkamal71@gmail.com>
2023-08-17 22:30:52 +0530

    Added

--- src/backendLink.jsx
+++ src/backendLink.jsx
@@ -1,5 +1,6 @@
-const backend = "https://solarbackend.onrender.com"
+// const backend = "https://solarbackend.onrender.com"
 // const backend = "http://localhost:5000"
+const backend = "https://solardealership.vercel.app"
 
 
 export default backend;
